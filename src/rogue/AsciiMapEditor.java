55cbbba40c25226ef1bacf0822025425643bf424
Daniel Kirchner <daniel@ekpyron.org>
2013-03-11 22:25:36 +0100

    Einfacher Copy&Paste-AsciiMapEditor für die Design-Crew.

---
+++ src/rogue/AsciiMapEditor.java
@@ -0,0 +1,89 @@
+package rogue;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.BadLocationException;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import jade.ui.TiledTermPanel;
+import jade.gen.map.AsciiMap;
+
+public class AsciiMapEditor implements DocumentListener {
+	
+	private TiledTermPanel term;
+	private JTextArea textarea;
+	
+	public AsciiMapEditor ()
+	{
+		term = new TiledTermPanel();
+		textarea = new JTextArea();
+		try {
+			Font font = Font.createFont(Font.TRUETYPE_FONT, new FileInputStream ("res/DejaVuSansMono.ttf"));
+	        textarea.setFont(font.deriveFont(Font.PLAIN, term.tileHeight()));
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(FontFormatException e) {
+			e.printStackTrace();
+		}
+		JFrame frame = new JFrame("AsciiMapEditor");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = frame.getContentPane();
+		c.setLayout (new GridLayout(0,2));
+		c.add (textarea);
+		c.add (term.panel());
+		frame.pack();
+		frame.setVisible(true);
+		
+		textarea.getDocument().addDocumentListener(this);
+	}
+	
+	public void changedUpdate(DocumentEvent e)
+	{
+		updateTermPanel();
+	}
+	
+	public void removeUpdate(DocumentEvent e)
+	{
+		updateTermPanel();
+	}
+	
+	public void insertUpdate(DocumentEvent e)
+	{
+		updateTermPanel();
+	}
+	
+	public void updateTermPanel()
+	{
+		try {
+		String content;
+		content = textarea.getDocument().getText(0, textarea.getDocument().getLength());
+		AsciiMap asciimap;
+		try {
+		 asciimap = AsciiMap.createFromString (content);
+		} catch (Exception e) {
+	        term.clearBuffer();
+	        term.bufferString(0,  0, "Invalid input");
+	        term.refreshScreen();
+			return;
+		}
+        term.clearBuffer();
+		asciimap.render (term);
+        term.refreshScreen();
+        } catch(BadLocationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AsciiMapEditor asciimapeditor = new AsciiMapEditor ();
+		
+
+	}
+
+}

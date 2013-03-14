a0961cb7e07332abfa4356cf97c7556867dbad81
Daniel Luetgehetmann <luetge@mi.fu-berlin.de>
2013-03-14 18:45:14 +0100

    Brains erweitert

--- src/pazi/features/Braaaiiiiins.java
+++ src/pazi/features/Braaaiiiiins.java
@@ -5,17 +5,23 @@ import rogue.creature.Monster;
 public class Braaaiiiiins implements IFeature<Monster> {
 
 	protected String text = "Braaaaaaiiiiiiiiiiiiiiiiiinns!!!";
+	protected int radius;
 	
 	public Braaaiiiiins(){}
 		
 	public Braaaiiiiins(String text){
+		this(text, 2);
+	}
+	
+	public Braaaiiiiins(String text, int radius){
 		this.text = text;
+		this.radius = radius;
 	}
 	
 	@Override
 	public void act(Monster monster) {
 		double dist = monster.world().getPlayer().pos().distance(monster.pos());
-		if(dist < 2 && Math.random() < 0.05)
+		if(dist < radius && Math.random() < 0.05)
 			monster.appendMessage(text);
 	}
 	

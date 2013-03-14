58da81ca95c42c585969a625e5cbc9e7584565af
Daniel Kirchner <daniel@ekpyron.org>
2013-03-14 15:40:53 +0100

    Einheitliche Richtungsfarben.

--- src/rogue/creature/CreatureFactory.java
+++ src/rogue/creature/CreatureFactory.java
@@ -19,15 +19,8 @@ public class CreatureFactory {
 		Creature creature = null;
 		if(identifier == "zombie1"){
 			ColoredChar faces[] = new ColoredChar[9];
-			faces[Direction.ORIGIN.getID()] = ColoredChar.create('Z', new Color(0x00FF00));
-			faces[Direction.NORTH.getID()] = ColoredChar.create('Z', new Color(0x01FF00));
-			faces[Direction.EAST.getID()] = ColoredChar.create('Z', new Color(0x02FF00));
-			faces[Direction.SOUTH.getID()] = ColoredChar.create('Z', new Color(0x03FF00));
-			faces[Direction.WEST.getID()] = ColoredChar.create('Z', new Color(0x04FF00));
-			faces[Direction.NORTHEAST.getID()] = ColoredChar.create('Z', new Color(0x05FF00));
-			faces[Direction.SOUTHEAST.getID()] = ColoredChar.create('Z', new Color(0x06FF00));
-			faces[Direction.SOUTHWEST.getID()] = ColoredChar.create('Z', new Color(0x07FF00));
-			faces[Direction.NORTHWEST.getID()] = ColoredChar.create('Z', new Color(0x08FF00));
+			for (int i = 0; i < 9; i++)
+				faces[i] = ColoredChar.create('Z', new Color(0x00FF00+i));
 			creature = new Monster(faces, "Blutiger Zombie");
 			creature.addGeneralFeature(Braaaiiiiins.getInstance());
 	        creature.setFightBehaviour(new DefaultFightBehaviour());

442602bba9dce13f8f9c00dfe1380e519380c870
Daniel Luetgehetmann <luetge@mi.fu-berlin.de>
2013-03-15 16:16:57 +0100

    README

--- src/jade/ui/Bagpack.java
+++ src/jade/ui/Bagpack.java
@@ -94,8 +94,10 @@ public class Bagpack {
 	}
 	
 	protected static void useItem() {
+		if(lstItems.getSelectedIndex() == -1)
+			return;
 		Guard.validateArgument(Player.class.isAssignableFrom(inventory.getOwner().getClass()));
-		Guard.validateArgument(inventory.getItems().size() > lstItems.getSelectedIndex() && lstItems.getSelectedIndex() > -1);
+		Guard.validateArgument(inventory.getItems().size() > lstItems.getSelectedIndex());
 		
 		((Player)inventory.getOwner()).useItem(inventory.getItems().get(lstItems.getSelectedIndex()));
 		sendDeadAcuteKey();
@@ -112,6 +114,8 @@ public class Bagpack {
 	}
 
 	protected static void showDescription() {
+		if(lstItems.getSelectedIndex() == -1)
+			return;
 		lblDescription.setText("Beschreibung:\n\n" + inventory.getItems().get(lstItems.getSelectedIndex()).getDescription());		
 	}
 

dfddbdc9388e380f38ed511e23dfd84c5757a366
Daniel Luetgehetmann <luetge@mi.fu-berlin.de>
2013-03-19 14:14:23 +0100

    weiter mit der shotgun, bald funktioniert sie

--- src/pazi/weapons/Shotgun.java
+++ src/pazi/weapons/Shotgun.java
@@ -1,24 +1,28 @@
 package pazi.weapons;
 
-import java.awt.Color;
-
 import jade.core.Actor;
 import jade.util.datatype.ColoredChar;
-import pazi.items.Item;
+import jade.util.datatype.Coordinate;
+
+import java.awt.Color;
+
+import com.sun.corba.se.impl.encoding.CodeSetConversion.BTCConverter;
+
 import rogue.creature.Creature;
+import rogue.creature.Player;
 
 public class Shotgun extends RCWeaponPrototype {
 
 	public Shotgun() {
-		
 		super(new ColoredChar('p', Color.lightGray), "Shotgun der Erlösung");
 		description = "Eine Waffe mit kurzer Reichweite und großer Streuung.";
 	}
 
 	@Override
 	public int getDamage(Creature attacker, Creature victim) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(attacker.pos().x() != victim.x() && attacker.pos().y() != victim.pos().y() || attacker.pos().distance(victim.pos()) > getRange())
+			return 0;
+		return 10;
 	}
 
 	@Override
@@ -28,13 +32,41 @@ public class Shotgun extends RCWeaponPrototype {
 
 	@Override
 	public void shoot(Creature attacker, Creature victim) {
-		// TODO Auto-generated method stub
-
+		if(Player.class.isAssignableFrom(attacker.getClass())) {
+			// Player-Verhalten
+		}
 	}
-
+	
+	protected Coordinate[] getHitCoordinates(Creature attacker, Creature victim) {
+		boolean bHorizontal = attacker.pos().y() == victim.pos().y();
+		return getHitCoordinates(attacker.pos(), bHorizontal, bHorizontal ? attacker.pos().x() < victim.pos().x() : attacker.pos().y() < victim.pos().y());
+	}
+	
+	protected Coordinate[] getHitCoordinates(Coordinate pos, boolean bHorizontal, boolean toTopRight) {
+		Coordinate[] coord = new Coordinate[9];
+		coord[0] = new Coordinate(1, 0);
+		
+		coord[1] = new Coordinate(2, 1);
+		coord[2] = new Coordinate(2, 0);
+		coord[3] = new Coordinate(2, -1);
+		
+		coord[4] = new Coordinate(3, 2);
+		coord[5] = new Coordinate(3, 1);
+		coord[6] = new Coordinate(3, 0);
+		coord[7] = new Coordinate(3, -1);
+		coord[8] = new Coordinate(3, -2);
+		
+		for(int i=0; i < 8; i++) {
+			coord[i] = (toTopRight ? coord[i] : coord[i].getTranslated(-2*coord[i].x(), 0));
+			coord[i] = (bHorizontal ? coord[i] : coord[i].getSwapped()).getTranslated(pos); 
+		}
+		
+		return coord;
+	}
+	
 	@Override
 	public double getRange() {
-		return 4;
+		return 3;
 	}
 
 	@Override

acee62d89582aa9fe5ff82fcb65f22f09b55cd12
Daniel Kirchner <daniel@ekpyron.org>
2013-03-21 14:07:45 +0100

    scientist0 läuft.

--- src/pazi/behaviour/AllyWalk.java
+++ src/pazi/behaviour/AllyWalk.java
@@ -1,5 +1,12 @@
 package pazi.behaviour;
 
+import jade.util.Dice;
+import jade.util.datatype.Coordinate;
+import jade.util.datatype.Direction;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
 import rogue.creature.Creature;
 
 public class AllyWalk implements IBehaviour<Creature> {
@@ -8,7 +15,15 @@ public class AllyWalk implements IBehaviour<Creature> {
 	public AllyWalk(){}
 	
 	public void act(Creature creature) {	
-			creature.walk();
+		if(creature.hasActed())
+			return;
+
+		if(Dice.global.chance(33)){
+			creature.move(Dice.global.choose(Arrays.asList(Direction.values())));
+			creature.setHasActed(true);
+			return;
+		}
+		creature.setHasActed(true);
 	}
 	@Override
 	public void exit(Creature actor) {}
